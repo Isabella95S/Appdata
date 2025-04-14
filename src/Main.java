68e89bf0a8f456fbc8ddff63a96535918d9f26d8
desan <desanctisisabella95@gmail.com>
2025-04-14 20:44:14 +0200

    commit app confronto tra date

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.Date;
-import java.util.Locale;
+import java.time.Instant;
+
 
 public class Main {
     public static void main(String[] args) {
-        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        //String dataString = data.getYear() + "-" + data.getMonth() + "-" + data.getDayOfMonth() + "-" + data.getDayOfWeek() ;
-        String formattedDate = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
-        //Offsetdatatime è un oggetto immutabile
-        OffsetDateTime data1 = data.plusYears(1).minusMonths(1).plusDays(7);
-        //formetter personalizzato
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss", Locale.ITALIAN);
+// Creazione dei due oggetti Instant dalle stringhe ISO 8601
+        Instant date1 = Instant.parse("2023-03-01T13:00:00Z");
+        Instant date2 = Instant.parse("2024-03-01T13:00:00Z");
 
-        System.out.println("data:" + formatter.format(data1));
+        // Verifica che la prima data è precedente alla seconda
+        boolean isDate1BeforeDate2 = date1.isBefore(date2);
+
+        // Verifica che la seconda data è successiva alla prima
+        boolean isDate2AfterDate1 = date2.isAfter(date1);
+
+        // Verifica che le due date sono uguali
+        boolean areDatesEqual = date1.equals(date2);
+
+        // Stampa i risultati
+        System.out.println("La prima data è precedente alla seconda? " + isDate1BeforeDate2);
+        System.out.println("La seconda data è successiva alla prima? " + isDate2AfterDate1);
+        System.out.println("Le due date sono uguali? " + areDatesEqual);
+
+
     }
 }
