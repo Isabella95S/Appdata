6d8a96caec404bdf752ce9ed78d3cef6920485a0
desan <desanctisisabella95@gmail.com>
2025-04-14 11:10:13 +0200

    secondo commit progetto

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,18 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Date;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        String dataString = data.getYear() + "-" + data.getMonth() + "-" + data.getDayOfMonth() + "-" + data.getDayOfWeek() ;
-        //String dataString = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
-        System.out.println("dataString: " + dataString);
+        //String dataString = data.getYear() + "-" + data.getMonth() + "-" + data.getDayOfMonth() + "-" + data.getDayOfWeek() ;
+        String formattedDate = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
+        //Offsetdatatime è un oggetto immutabile
+        OffsetDateTime data1 = data.plusYears(1).minusMonths(1).plusDays(7);
+        //formetter personalizzato
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss", Locale.ITALIAN);
+
+        System.out.println("data:" + formatter.format(data1));
     }
 }
